68890f4b72d5ea7a3abeb11818086f0455ed6a50
WU.SJ <wsj0898@gmail.com>
2014-10-24 00:17:27 +0800

    Complete getCode, but notfile out and file in

--- hfTree.h
+++ hfTree.h
@@ -3,6 +3,7 @@
 
 #include <string>
 using namespace std;
+
 template<class T>
 class hfTree
 {
@@ -26,6 +27,8 @@ public:
 
 	hfTree(const T *x, const int *w, int size);
 	void getCode(hfCode result[]);
+	//void getData(hfCode result[], string *myCode);
+	void getData(hfCode result[], string mycode[], int mySize);
 	~hfTree()
 	{
 		delete[] elem;
@@ -50,7 +53,7 @@ hfTree<T>::hfTree(const T *v, const int *w, int size)
 		elem[i].right = 0;
 	}
 
-	//gui bing
+	//归并
 	for (int i = size -1; i > 0; --i)
 	{
 		min1 = MAX_INT;
@@ -89,6 +92,7 @@ hfTree<T>::hfTree(const T *v, const int *w, int size)
 template<class T>
 void hfTree<T>::getCode(hfCode result[])
 {
+    cout  << "开始编码" << endl;
     int size = length / 2;
     int p, s;
     for (int i = size; i < length; ++i)
@@ -109,5 +113,46 @@ void hfTree<T>::getCode(hfCode result[])
         }
     }
 }
+
+template<class T>
+void hfTree<T>::getData(hfCode result[], string mycode[], int mySize)
+{
+    cout << "开始解码" << endl;
+    for (int i = 0; i < mySize; i++)
+    {
+        result[i].code = mycode[i];
+    }
+    //从根节点开始寻找，遇到0往左，遇到1往右
+
+    for (int i = 0; i < mySize; i++)
+    {
+        int codeMaxSize = result[i].code.size();
+        //cout << codeMaxSize << endl;
+        //寻找数据下标
+        int s = 1;
+        //指向当前编码中一个数字的下标
+        int n = 0;
+        while (n < codeMaxSize)
+        {
+            if (result[i].code[n] == '0')
+                s = elem[s].left;
+            else
+                s = elem[s].right;
+            //cout << "s is " << s << endl;
+            n++;
+        }
+
+        result[i].data = elem[s].data;
+    }
+    return;
+}
+
+/*
+template<class T>
+void hfTree<T>::getData(hfCode result, string *mycode)
+{
+        return;
+}
+*/
 
 #endif // _hfmanTree
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //Name 吴松骏
 #include <iostream>
+#include <string>
 #include "hfTree.h"
 
 using namespace std;
@@ -17,7 +18,17 @@ int main()
 
     for (int i = 0; i < 7; i++)
     {
-        cout << result[i].data << " " << result[i].code << endl;
+        cout << result[i].data << "\t\t" << result[i].code << endl;
+    }
+
+    hfTree<char>::hfCode result2[7];
+    string numb[7] = {"000", "01", "11", "00110", "0010", "10", "00111"};
+    //cout << numb.size();
+
+    tree.getData(result2, numb, 7);
+    for (int i = 0; i < 7; i++)
+    {
+        cout << result2[i].code << "\t\t" << result2[i].data << endl;
     }
     return 0;
 }
